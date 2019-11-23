acc203f80a05f897ec65a326a465c69aaabc277a
Mike-Fernandez <00145518@uca.edu.sv>
2019-11-23 09:09:06 -0600

    Modified first view and shaping the website

--- models/beca.js
+++ models/beca.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 var becaSchema = Schema({
     nombre: { type: String, required: true },
     fundacion: { type: String, required: true },
-    tipo: { type: String, enum: ['completa', 'parcial', 'de matricula', 'residencia', 'transporte'], required: true },
+    tipo: { type: String, enum: ['completa', 'parcial', 'matricula', 'residencia', 'transporte'], required: true },
     descripcion: String
 }, {collection: 'Beca'});
 
